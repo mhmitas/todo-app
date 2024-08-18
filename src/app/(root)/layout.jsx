7a3dcdff835cb1a7f74178cc4ae4c9bc82ef454b
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-18 20:34:39 +0600

    hello

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const layout = ({ children }) => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col w-full'>
             <Navbar />
-            <section className='max-w-screen-2xl mx-auto flex-1'>
+            <section className='max-w-screen-2xl mx-auto flex-1 mt-16 w-full'>
                 {children}
             </section>
         </div>
--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section>
-
+    <section className="mt-4 space-y-10">
+      <Link href="https://mail.google.com/mail/?tab=rm&ogbl">
+        <div className="text-4xl font-black flex items-center justify-center gap-2">
+          <Image src={"/assets/gamil.png"} height={50} width={50} alt="Gmail-logo" />
+          <span>Check Mail</span>
+        </div>
+      </Link>
     </section>
   );
 }
