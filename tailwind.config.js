17228be0852242df87f19f1d7419fadde94a9ec8
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-20 07:09:33 +0600

    iso change

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        border: 'borderAnimation 5s linear infinite',
+      },
+      keyframes: {
+        borderAnimation: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '100%': { backgroundPosition: '100% 50%' },
+        },
       },
     },
   },
