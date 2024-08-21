018a567447511bc3ffddc25444b5dd3a21065e0b
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-21 20:15:03 +0600

    init shadcs and daisyui

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,15 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="mt-4 space-y-10">
+    <section className="mt-4 space-y-10 my-container">
       <Link href="https://mail.google.com/mail/?tab=rm&ogbl">
         <div className="text-4xl font-black flex items-center justify-center gap-2">
           <Image src={"/assets/gamil.png"} height={50} width={50} alt="Gmail-logo" />
           <span>Check Mail</span>
         </div>
       </Link>
+      <button className="btn btn-sm btn-success">DaisyUI</button>
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {
-      animation: {
-        border: 'borderAnimation 5s linear infinite',
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       keyframes: {
-        borderAnimation: {
-          '0%': { backgroundPosition: '0% 50%' },
-          '100%': { backgroundPosition: '100% 50%' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#52525b",
+          primary: "#FFFFFF",
+          info: "#3b82f6",
+          success: "#272d1d",
+          "success-content": "#bef264",
+          neutral: "#52525b",
+          "base-100": "#09090b",
+          "base-200": "#18181b",
+          "base-300": "#242427",
+          "base-content": "#ffffff"
+        }
+      }
+    ]
+  },
+  plugins: [require("tailwindcss-animate"), require("daisyui")],
+}
